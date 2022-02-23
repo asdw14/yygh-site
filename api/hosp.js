b2642e8b1c94844c6912d6f24baf422bd9009f5a
dizhongdi <2755063993@qq.com>
2022-02-23 23:02:43 +0800

    预约挂号部分页面

--- api/hosp.js
+++ api/hosp.js
@@ -30,8 +30,30 @@ export default {
             url: `${api_name}/department/${hoscode}`,
             method: 'get'
         })
-    }
+    },
+  // 获取可预约排班数据
+  getBookingScheduleRule(page, limit, hoscode, depcode) {
+    return request({
+        url: `${api_name}/auth/getBookingScheduleRule/${page}/${limit}/${hoscode}/${depcode}`,
+        method: 'get'
+    })
+},
+    // 获取排班数据
+findScheduleList(hoscode, depcode, workDate) {
+    return request({
+        url: `${api_name}/auth/findScheduleList/${hoscode}/${depcode}/${workDate}`,
+        method: 'get'
+    })
+},
 
+//根据id获取排班数据
+getSchedule(id) {
+    return request({
+      url: `${api_name}/getSchedule/${id}`,
+      method: 'get'
+    })
+    }
+    
 
 
 }
