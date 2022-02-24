93e11413f67ddbce2078406a236be7149d4ee8b1
dizhongdi <2755063993@qq.com>
2022-02-24 23:32:25 +0800

    订单

--- api/order/orderInfo.js
+++ api/order/orderInfo.js
@@ -8,5 +8,29 @@ export default {
     url: `${api_name}/auth/submitOrder/${scheduleId}/${patientId}`,
     method: 'post'
   })
- }
+ },
+ //订单列表
+getPageList(page, limit, searchObj) {
+  return request({
+      url: `${api_name}/auth/${page}/${limit}`,
+      method: `get`,
+      params: searchObj
+  })
+},
+//订单状态
+getStatusList() {
+  return request({
+      url: `${api_name}/auth/getStatusList`,
+      method: 'get'
+  })
+},
+    //订单详情
+    getOrders(orderId) {
+      return request({
+          url: `${api_name}/auth/getOrders/${orderId}`,
+          method: `get`
+      })
+  },
+
+
 }
